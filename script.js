da2846a9bf61510bcec8e87ed20b7e66eb75db7a
Sukiih <vale.sgonzalez2011@hotmail.com>
2025-01-03 23:15:17 -0300

    ADD: Añade dibujo, lógica y control de movimiento del soporte + movimiento de la pelotita.

--- script.js
+++ script.js
@@ -14,16 +14,35 @@ let y = canvas.height - 30;
 let dx = 2;  //eje horizontal
 let dy = -2; //eje vertical
 
+//variables plataforma
+const paddleHeight = 10;
+const paddleWidth = 50;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight - 10;
+
+//movimiento plataforma
+let rigthPressed = false;
+let leftPressed = false;
+
+const paddleSensitivity = 7;
 
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#fff";
     ctx.fill();
-    ctx.closePath();  //optimizamos rendimiento
+    ctx.closePath();
 }
 
-function drawPaddle(){}
+function drawPaddle(){
+    ctx.fillRect(
+        paddleX,
+        paddleY,
+        paddleWidth,
+        paddleHeight
+    )
+}
 
 function drawBricks(){}
 
@@ -31,15 +50,68 @@ function drawBricks(){}
 function collisionDetection(){}
 
 function ballMovement(){
+    //rebotar bolita en los laterales
+    if(
+        x + dx > canvas.width - ballRadius || //parte derecha
+        x + dx < ballRadius //parte izquierda
+    ){
+        dx = -dx
+    }
+
+    //rebotar bolita parte superior
+    if(y + dy < ballRadius){ //parte superior
+        dy = -dy
+    }
+
+    //si la bolita toca el suelo
+    if (y + dy > canvas.height - ballRadius){ 
+        console.log("Perdiste");
+        document.location.reload(); 
+    }
+
+
     x += dx;
     y += dy;
 }
 
-function paddleMovement(){}
+function paddleMovement(){
+    if (rigthPressed && paddleX < canvas.width - paddleWidth){
+        paddleX += paddleSensitivity;
+    } else if (leftPressed && paddleX > 0){
+        paddleX -= paddleSensitivity;
+    }
+}
 
 function cleanCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function initEvents (){
+    document.addEventListener("keydown", keyDownHandler);
+    document.addEventListener("keyup", keyUpHandler);
+
+    function keyDownHandler(event){
+        const {key} = event;
+
+        if(key === "Right" || key === "ArrowRight"){
+            rigthPressed = true;
+        }else if(key === "Left" || key === "ArrowLeft"){
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(event){
+        const {key} = event;
+
+        if(key === "Right" || key === "ArrowRight"){
+            rigthPressed = false;
+        }else if(key === "Left" || key === "ArrowLeft"){
+            leftPressed = false;
+        }
+    }
+}
+
+
 
 
 function draw (){
@@ -60,3 +132,4 @@ function draw (){
 }
 
 draw();
+initEvents();
